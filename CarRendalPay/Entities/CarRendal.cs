fa60bb768222ba0f11b6c6014f9d0d7333c067c4
Joao Vitor <githubconta@gmail.com>
2023-12-27 00:07:35 -0300

    Fisrt commit

---
+++ CarRendalPay/Entities/CarRendal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarRendalPay.Entities
+{
+    internal class CarRendal
+    {
+        // relacionamente... um vehicle e um invoice 
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Invoice Invoice { get; set; }
+        public Vehicle Vehicle { get; set; }
+
+        public CarRendal(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null; 
+        } 
+    }
+}
---
+++ CarRendalPay/Program.cs
@@ -0,0 +1,40 @@
+﻿using CarRendalPay.Entities;
+using System.Globalization; 
+
+namespace CarRendalPay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  entrada de dados 
+
+            Console.WriteLine("Enter rental data");
+            
+            Console.Write("Car Model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm:ss):  ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture );
+
+            Console.Write("Return (dd/MM/yyyy hh:mm:ss):  ");
+            DateTime finifh = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            // instaciamente do objeto CarRenatl 
+
+            CarRendal carRendal = new CarRendal(start, finifh, new Vehicle(model)); 
+
+
+
+
+            Console.Write("Enter price per hour: ");
+
+            Console.Write("Enter price per day: "); 
+
+            Console.WriteLine("INVOICE");
+            Console.WriteLine(); 
+    
+            // saida de dados 
+        }
+    }
+}
